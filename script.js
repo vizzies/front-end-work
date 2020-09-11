db7850302fbf698eed2579dfedd9ccfc8a6fa5b4
Kelci Mensah <kelcimensah@outlook.com>
2020-09-11 14:59:57 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function test(evt) {
+  const Http = new XMLHttpRequest();
+  const url='https://jsonplaceholder.typicode.com/posts';
+  Http.open("GET", url);
+  Http.send();
+
+  Http.onreadystatechange = (e) => {
+    console.log(Http.responseText)
+  }
+}
